f5e629cfdec2bc40b7386609adc36796961ad852
Andrew Lechlak <AndrewLechlak@gmail.com>
2024-12-22 10:14:32 -0500

    test

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,7 +2,7 @@ export const About = () => {
   return (
     <section className="relative">
       <div className="max-w-6xl mx-auto">
-        <h2 className="text-4xl font-bold mb-8 text-center bg-gradient-to-r from-cyan-500 to-sky-800 bg-clip-text text-transparent section-title">
+        <h2 className="text-4xl font-bold mb-8 text-center bg-gradient-to-r from-cyan-400 to-sky-500 bg-clip-text text-transparent section-title">
           About Me
         </h2>
         <div className="glass-card p-8">
