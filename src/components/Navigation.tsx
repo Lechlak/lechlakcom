4d4ddbaef16655d6954f353314f9480437ef20d6
Andrew Lechlak <AndrewLechlak@gmail.com>
2024-12-29 09:53:20 -0500

    triforce

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -93,11 +93,7 @@ export const Navigation = () => {
           <div className="flex-shrink-0 flex items-center">
             <div className="relative flex items-center">
             <div
-                className="w-16 h-16 bg-sky-800 transform"
-                style={{
-                  clipPath: "polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)",
-                  transform: `rotate(${rotation}deg)`,
-                }}
+                className="w-4 h-4 bg-amber-400 transform rotating-icon triforce"
               />
               <span className="ml-2 text-xl font-bold text-sky-700">AL</span>
             </div>
